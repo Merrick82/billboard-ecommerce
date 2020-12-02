8f5f3ec1547c68e1d0a5abacce1dab603202df3e
Merrick <federico.tarasco@gmail.com>
2020-12-02 00:02:28 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
+import appLogo from './assets/icons/billboard-logo.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={appLogo} className="App-logo" alt="logo" />
+        <h2>
+          Bienvenido a Billboard-eCommerce
+        </h2>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          El lugar dónde encontrarás películas, series, recitales y más!!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
